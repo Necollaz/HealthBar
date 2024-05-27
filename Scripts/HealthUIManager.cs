5ae8f76e8a2eb501d4a8e91b7b7a8965955c4da3
Necollaz <156367647+Necollaz@users.noreply.github.com>
2024-05-27 12:07:24 +0300

    2

--- Scripts/HealthUIManager.cs
+++ Scripts/HealthUIManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +6,6 @@ public class HealthUIManager : MonoBehaviour
     [SerializeField] private Health _health;
     [SerializeField] private Text _healthText;
     [SerializeField] private Slider _healthBar;
-    [SerializeField] private Slider _smoothHealthBar;
-    [SerializeField] private float _smoothSpeed = 5f;
 
     private void OnEnable()
     {
@@ -29,17 +26,5 @@ public class HealthUIManager : MonoBehaviour
     {
         _healthText.text = $"{_health.CurrentHealth} / {_health.MaxHealth}";
         _healthBar.value = (float)_health.CurrentHealth / _health.MaxHealth;
-        StartCoroutine(UpdateSmoothHealthBar());
-    }
-
-    private IEnumerator UpdateSmoothHealthBar()
-    {
-        while(Mathf.Abs(_smoothHealthBar.value - (float)_health.CurrentHealth / _health.MaxHealth) > 0.01f)
-        {
-            _smoothHealthBar.value = Mathf.MoveTowards(_smoothHealthBar.value, (float)_health.CurrentHealth / _health.MaxHealth, Time.deltaTime * _smoothSpeed);
-            yield return null;
-        }
-
-        _smoothHealthBar.value = (float)_health.CurrentHealth / _health.MaxHealth;
     }
 }
